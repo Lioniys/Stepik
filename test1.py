a2f93b3059e971423becf21fd67e3132623b42db
Lioniys <lioniys1998@gmail.com>
2022-09-04 22:50:54 +0300

    a

--- test1.py
+++ test1.py
@@ -1,9 +1,12 @@
 class Data:
-    def __init__(self,data,ip) -> None:
+    def __init__(self, data, ip):
+        """
+        data - передаваемые данные (строка);
+        ip - IP-адрес назначения.
+        """
         self.data = data
         self.ip = ip
-        pass
-
+        
 
 class Server:
     counter_server = 1
@@ -12,45 +15,78 @@ class Server:
         cls.counter_server += 1
         return super().__new__(cls)
 
-    def __init__(self) -> None:
+    def __init__(self):
+        """
+        buffer - список принятых пакетов;
+        ip - IP-адрес текущего сервера.
+        """
         self.buffer = []
         self.ip = self.counter_server
         self.router = None
-        pass
-
-    def send_data(self,data):
+        
+    def send_data(self, data):
+        """
+        Для отправки информационного пакета data 
+        с указанным IP-адресом получателя 
+        (пакет отправляется роутеру и
+        сохраняется в его buffer)
+        """
         if self.ip in self.router.servers_ip:
             self.router.buffer.append(data)
-        pass
-
-    def get_data(self):
-        a = self.buffer[:]
-        self.buffer.clear()
-        return a
         
-
+    def get_data(self):
+        """
+        Возвращает список принятых пакетов 
+        и очищает входной буфер
+        """
+        lst = self.buffer[:]
+        self.buffer.clear()
+        return lst
+        
     def get_ip(self):
+        """
+        Возвращает свой IP-адрес.
+        """
         return self.ip        
 
 
 class Router:
-    def __init__(self) -> None:
+    def __init__(self):
+        """
+        Для описания работы роутеров в сети
+        (в данной задаче полагается один роутер).
+        buffer - список для хранения принятых от
+        серверов пакетов (объектов класса Data).
+        """
         self.buffer = []
         self.servers_ip = {}
-        pass
-    
-    def link(self,server):
+        
+    def link(self, server):
+        """
+        Для присоединения сервера 
+        (объекта класса Server) 
+        к роутеру
+        """
         self.servers_ip[server.ip] = server
         server.router = self
-        pass
-    
-    def unlink(self,server):
+        
+    def unlink(self, server):
+        """
+        Для отсоединения сервера 
+        (объекта класса Server) 
+        от роутера
+        """
         self.servers_ip.pop(server.ip)
-        pass
-    
+        
     def send_data(self):
+        """
+        Для отправки всех пакетов 
+        (объектов класса Data) 
+        из буфера роутера
+        соответствующим серверам 
+        (после отправки буфер должен очищаться)
+        """
         for x in self.buffer:
             if x.ip in self.servers_ip:
                 self.servers_ip[x.ip].buffer.append(x)
         self.buffer.clear()
-        pass
--- test3.py
+++ test3.py
@@ -1,6 +1,71 @@
-#односвязный список
+class Stack:
+    def __init__(self) -> None:
+        self.top = None
+        self.last = None
+        pass
+
+    def __add__(self, other):
+        """
+        Добавление нового
+        объекта класса StackObj
+        в конец односвязного списка
+        """
+        self.push_back(other)
+        return self
+
+    def __mul__(self, other: list):
+        """
+        Добавление нескольких объектов
+        в конец односвязного списка
+        """
+        for i in [StackObj(x) for x in other]:
+            self.push_back(i)
+        return self
+
+    def push_back(self, obj):
+        """
+        Добавляет элемент
+        в конец стека
+        """
+        if self.top is None:
+            self.top = obj
+        else:
+            self.last.next = obj
+        self.last = obj
+
+    def pop_back(self):
+        """
+        Удаляет последний елемент
+        и возвращяет его
+        """
+        top = self.top
+        if top is not None:
+            if top.next is None:
+                self.top = None
+                return self.last
+            else:
+                while top.next.next is not None:
+                    top = top.next
+                top.next = None
+                return self.last
+
+    def get_data(self):
+        """
+        Возвращяет список данных
+        из всех елементов стека
+        """
+        lst = []
+        top = self.top
+        if top is not None:
+            while top.next is not None:
+                lst.append(top.data)
+                top = top.next
+            lst.append(top.data)
+        return lst
+
+
 class StackObj:
-    def __init__(self,data) -> None:
+    def __init__(self, data) -> None:
         self.__data = data
         self.__next = None
         pass
@@ -10,52 +75,15 @@ class StackObj:
         return self.__data
 
     @data.setter
-    def data(self,data):
-        if isinstance(data,StackObj) or next == None:
+    def data(self, data):
+        if isinstance(data, StackObj) or next is None:
             self.__data = data
 
     @property
     def next(self):
         return self.__next
-    
+
     @next.setter
-    def next(self,next):
-        if isinstance(next,StackObj) or next == None:
-            self.__next = next
-
-
-class Stack:
-    def __init__(self) -> None:
-        self.top = None
-        self.last = None
-        pass
-
-    def push(self, obj):
-        if self.top == None:
-            self.top = obj
-        else:
-            self.last.next = obj
-        self.last = obj
-        pass
-
-    def pop(self):
-        i = self.top
-        if i != None:
-            if i.next == None:
-                self.top = None
-                return self.last
-            else:
-                while i.next.next != None:
-                    i = i.next
-                i.next = None
-                return self.last  
-
-    def get_data(self):
-        l = []
-        x = self.top
-        if x != None:
-            while x.next != None:
-                l.append(x.data)
-                x = x.next
-            l.append(x.data)
-        return l
+    def next(self, obj):
+        if isinstance(obj, StackObj) or obj is None:
+            self.__next = obj
