c8fa1b4abc7751fc94cdbf267982e6fe7874ace1
Lioniys <lioniys1998@gmail.com>
2022-09-04 23:26:21 +0300

    a

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,6 +1,48 @@
-#2.2 подвиг 8 Решающее дерево
+class DecisionTree:
+    """
+    Для работы с решающим деревом в целом
+    """
+    @classmethod
+    def predict(cls, root, x):
+        """
+        Для построения прогноза 
+        (прохода по решающему дереву) 
+        для вектора x из корневого узла дерева root.
+        """
+        while root.value is None:
+            if x[root.indx] == 1:
+                root = root.left
+            else:
+                root = root.right
+        return root.value
+
+    @classmethod
+    def add_obj(cls, obj, node=None, left=True):
+        """
+        Для добавления вершин 
+        в решающее дерево
+        """
+        if node is None:
+            return obj
+        if left:
+            node.left = obj
+            return obj
+        else:
+            node.right = obj
+            return obj
+
+
 class TreeObj:
+    """
+    Описание вершин и листьев решающего дерева
+    """
     def __init__(self, indx, value=None):
+        """
+        indx - проверяемый индекс (целое число);
+        value - значение с данными (строка);
+        __left - ссылка на следующий объект дерева по левой ветви;
+        __right - ссылка на следующий объект дерева по правой ветв.
+        """
         self.indx = indx
         self.value = value
         self.__left = None
@@ -21,25 +63,3 @@ class TreeObj:
     @right.setter
     def right(self,right):
         self.__right = right
-
-
-class DecisionTree:
-    @classmethod
-    def predict(cls, root, x):
-        while root.value == None:
-            if x[root.indx] == 1:
-                root = root.left
-            else:
-                root = root.right
-        return root.value
-
-    @classmethod
-    def add_obj(cls, obj, node=None, left=True):
-        if node == None:
-            return obj
-        if left:
-            node.left = obj
-            return obj
-        else:
-            node.right = obj
-            return obj
--- test0.py
+++ test0.py
@@ -1,24 +1,28 @@
 from random import randint
 
-# Сапер
-class Cell:
-    def __init__(self, around_mines=0, mine=False) -> None:
-        self.around_mines = around_mines
-        self.mine = mine
-        self.fl_open = False
-        pass
-
-
 class GamePole:
-    def __init__(self, N, M) -> None:
+    """
+    Управление игровым полем, 
+    размером N x N клеток
+    """
+    def __init__(self, N, M):
+        """
+        N - размер поля; 
+        M - общее число мин на поле.
+        каждая клетка представляется объектом класса Cell 
+        и все объекты хранятся в двумерном списке N x N элементов
+        """
         self.n = N
         self.m = M
         self.pole = [[Cell() for x in range(self.n)] for y in range(self.n)]
         self.init()
         self.counter()
-        pass
-
+        
     def init(self):
+        """
+        Инициализация поля 
+        с новой расстановкой M мин
+        """
         m = 0
         while m < self.m:
             x = randint(0, self.n-1)
@@ -30,6 +34,9 @@ class GamePole:
             m += 1
 
     def counter(self):
+        """
+        Подсчет мин вокруг клетки
+        """
         indx = (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)
         for x in range(self.n):
             for y in range(self.n):
@@ -38,9 +45,24 @@ class GamePole:
                     self.pole[x][y].around_mines = mines
 
     def show(self):
+        """
+        Отображение поля в консоли 
+        в виде таблицы чисел открытых клеток
+        """
         for x in self.pole:
             print(*map(lambda x: "#" if not x.fl_open else x.around_mines if not x.mine else "*", x))
 
 
-pole_game = GamePole(10, 12)
-pole_game.show()
+class Cell:
+    """
+    Представление клетки игрового поля
+    """
+    def __init__(self, around_mines=0, mine=False):
+        """
+        around_mines - число мин вокруг клетки;
+        mine - наличие мины в текущей клетке;
+        fl_open - открыта/закрыта клетка.
+        """
+        self.around_mines = around_mines
+        self.mine = mine
+        self.fl_open = False
